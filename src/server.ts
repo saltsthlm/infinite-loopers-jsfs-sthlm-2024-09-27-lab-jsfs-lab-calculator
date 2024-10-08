1058f15e8ae6fbf086f85c7d3bfce14a9f298d08
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-10-08 14:14:02 +0200

    all tests pass
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    
    Co-authored-by: Love Bellinder <love.bellinder@gmail.com>
    
    Co-authored-by: Adrienn Pozsgai <apozsgai97@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const server = http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     // res.end(greeter.hello);
-    // test test
+    // test
   })
   .listen(port);
 
