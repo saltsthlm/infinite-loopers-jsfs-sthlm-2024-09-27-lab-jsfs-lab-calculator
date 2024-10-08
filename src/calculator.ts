5cea3c0f92dbe029893114cd6584b653e6305d68
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-08 12:05:57 +0200

    Co-authored-by: Adrienn <apozsgai97@gmail.com> Co-authored-by: love bellinder <love.bellinder@gmail.com> Co-authored-by: Ali <ali.mohseni05@yahoo.se>

---
+++ src/calculator.ts
@@ -0,0 +1,13 @@
+function evaluate(inp: string) {
+  const arr = inp.split("+", 10);
+  const a = arr[0];
+  const b = arr[arr.length - 1];
+  console.log(addition(parseInt(a), parseInt(b)));
+  console.log({ a });
+  console.log({ b });
+}
+function addition(a: number, b: number) {
+  return a + b;
+}
+
+evaluate("40+50");
