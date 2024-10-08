e54e691b0144a84b2a7d2857af8c9c95bf1ced59
defycab <love.bellinder@gmail.com>
2024-10-08 14:35:31 +0200

    add tests for evaluate function. all four green.
    Co-authored-by: Adrienn <apozsgai97@gmail.com>
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,5 +1,12 @@
-import { addition, subtraction, multiplication, division } from "./calculator";
-describe("Evaluate", () => {
+import {
+  addition,
+  subtraction,
+  multiplication,
+  division,
+  evaluate,
+} from "./calculator";
+
+describe("Function", () => {
   it("should add numbers", () => {
     const result = addition(4, 5);
     expect(result).toBe(9);
@@ -14,6 +21,25 @@ describe("Evaluate", () => {
   });
   it("should divsion numbers", () => {
     const result = division(4, 2);
+    expect(result).toBe(2);
+  });
+});
+
+describe("Evaluate", () => {
+  it("should add numbers", () => {
+    const result = evaluate("4+5");
+    expect(result).toBe(9);
+  });
+  it("should subtract numbers", () => {
+    const result = evaluate("4-5");
+    expect(result).toBe(-1);
+  });
+  it("should multiply numbers", () => {
+    const result = evaluate("4*5");
+    expect(result).toBe(20);
+  });
+  it("should divsion numbers", () => {
+    const result = evaluate("4/2");
     expect(result).toBe(2);
   });
 });
