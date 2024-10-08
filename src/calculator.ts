0d9b115807da37d3c0a6bd09a0fd7e5d2d638c24
defycab <love.bellinder@gmail.com>
2024-10-08 13:36:34 +0200

    collect input methods
    Co-authored-by: Adrienn <apozsgai97@gmail.com>
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,31 +1,39 @@
 import { throwDeprecation } from "process";
 
 function evaluate(inp: string) {
+  const collectOperator = inp.match(/[+-/*]/);
+  if (!collectOperator || collectOperator.length > 1) {
+    throw new Error("Wrong format");
+  }
+
+  const operator = collectOperator[0];
+
   const arr = inp.split("+", 10);
-  const a = arr[0];
-  const b = arr[arr.length - 1];
+  const a = parseInt(arr[0]);
+  const b = parseInt(arr[arr.length - 1]);
 
   let result = 0;
 
   switch (operator) {
     case "+":
-      result = addition(a,b);
+      result = addition(a, b);
       break;
     case "-":
-      result = subtraction(a,b);
+      result = subtraction(a, b);
       break;
     case "*":
-      result = multiplication(a,b);
+      result = multiplication(a, b);
       break;
     case "/":
-      result = division(a,b);
+      result = division(a, b);
       break;
 
     default:
-      throw new Error ("Faulty input");
+      throw new Error("Faulty input");
   }
   return result;
 }
+
 function addition(a: number, b: number) {
   return a + b;
 }
@@ -39,4 +47,4 @@ function division(a: number, b: number) {
   return a / b;
 }
 
-evaluate("40+50");
+console.log(evaluate("40+50"));
