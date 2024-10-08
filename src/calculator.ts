7ddd95079a0a03dbbb564505ecb790c4bee52950
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-08 13:27:20 +0200

    Add switch case for operator
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,10 +1,30 @@
+import { throwDeprecation } from "process";
+
 function evaluate(inp: string) {
   const arr = inp.split("+", 10);
   const a = arr[0];
   const b = arr[arr.length - 1];
-  console.log(addition(parseInt(a), parseInt(b)));
-  console.log({ a });
-  console.log({ b });
+
+  let result = 0;
+
+  switch (operator) {
+    case "+":
+      result = addition(a,b);
+      break;
+    case "-":
+      result = subtraction(a,b);
+      break;
+    case "*":
+      result = multiplication(a,b);
+      break;
+    case "/":
+      result = division(a,b);
+      break;
+
+    default:
+      throw new Error ("Faulty input");
+  }
+  return result;
 }
 function addition(a: number, b: number) {
   return a + b;
