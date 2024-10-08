b6301e15009034699cedf9957591441299baf8eb
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-10-08 14:10:59 +0200

    all tests pass
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    
    Co-authored-by: Love Bellinder <love.bellinder@gmail.com>
    
    Co-authored-by: Adrienn Pozsgai <apozsgai97@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import http from 'http';
+import http from "http";
 // import greeter from './greeter';
-
 
 const port = 8080;
 
 const server = http
   .createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { "Content-Type": "text/plain" });
     // res.end(greeter.hello);
+    // test test
   })
   .listen(port);
 
-server.on('listening', () => console.log('Server is listening on port', server.address()));
+server.on("listening", () =>
+  console.log("Server is listening on port", server.address())
+);
