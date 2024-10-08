200098b7160ffa536c75822a5e1e7b5332a87ad2
defycab <love.bellinder@gmail.com>
2024-10-08 11:43:12 +0200

    add test for different methods of calculations
    Co-authored-by: Adrienn <apozsgai97@gmail.com>
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,6 +1,18 @@
 describe("Evaluate", () => {
   it("should add numbers", () => {
-    const result = addition("4 + 5");
+    const result = addition(4, 5);
     expect(result).toBe(9);
+  });
+  it("should subtract numbers", () => {
+    const result = subtraction(4, 5);
+    expect(result).toBe(-1);
+  });
+  it("should multiply numbers", () => {
+    const result = multiplication(4, 5);
+    expect(result).toBe(20);
+  });
+  it("should divsion numbers", () => {
+    const result = divison(4, 2);
+    expect(result).toBe(2);
   });
 });
--- src/greeter.test.ts
+++
@@ -1,7 +0,0 @@
-import greeter from "./greeter";
-
-describe("Greeter", () => {
-  it("should greet", () => {
-    expect(greeter.hello).toBe("Hello salt!");
-  });
-});
