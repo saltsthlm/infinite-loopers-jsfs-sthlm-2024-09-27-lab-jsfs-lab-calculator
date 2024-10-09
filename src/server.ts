9b345594050f552833924ccc588dbbcfd81e88ba
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-10-09 14:14:51 +0200

    added first part of api
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import http from "http";
-// import greeter from './greeter';
+import greeter from "./greeter";
 
 const port = 8080;
 
 const server = http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    // res.end(greeter.hello);
-    // test
+    res.end(greeter.hello);
   })
   .listen(port);
 
